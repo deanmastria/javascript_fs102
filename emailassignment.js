437a7ccc76a98c6d839964559eb6ca30c27ed138
deanmastria <mastriadata@gmail.com>
2024-06-11 19:42:02 -0400

    email outline

---
+++ emailassignment.js
@@ -0,0 +1,23 @@
+let emaildata = {
+        name: gmail
+        mailboxes:[
+            'inbox', 
+            'junk', 
+            'sent', 
+            'spam', 
+            'archived', 
+        ],
+        contacts: [ 
+            {name: 'Mike', lastMessage: "Good seeing you today!"},
+            {name: 'Jeff', lastMessage: "Beach later, be there!"},
+            {name: 'Lenny', lastMessage: "We've been trying to reach you about your cars warranty!"},
+            {name: 'Jon', lastMessage: "Send the docs over."},
+            {name: 'Chris', lastMessage: "I'm moving."}, 
+        ],
+        emails: [
+            '@toyota.com',
+            '@gucci.com',
+            '@apple.com',
+            '@overthere.com',
+        ]
+        
