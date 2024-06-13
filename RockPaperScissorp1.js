4c4437a61362f92400bd7516b13b16602363ebc5
deanmastria <mastriadata@gmail.com>
2024-06-13 14:18:50 -0400

    testing

--- RockPaperScissorp1.js
+++ RockPaperScissorp1.js
@@ -1,17 +1,18 @@
-const hands = ['rock', 'paper', 'scissors']; //an array that creates rock papaer scissors as the constant variables
+const hands = ['rock', 'paper', 'scissors'];                            //an array that creates rock papaer scissors as the constant variables
+
 
 function getHand(){
-    return hands[parseInt(Math.random()*10)%3] //returns hands from the array
+    return hands[parseInt(Math.random()*10)%3]                          //returns hands from the array
 }
 
-let players = {                               //variable
-    deano: {                                  //object 1
-        name: 'deano',                        //property 1
-        getHand: getHand                      //property 2
+let players = {                                                         //variable
+    deano: {                                                            //object 1
+        name: 'deano',                                                  //property 1
+        getHand: getHand                                                //property 2
     },
-    jack: {                                   //object 2
-        name: 'jack',                          //property 1
-        getHand: getHand                      //property 2
+    jack: {                                                             //object 2
+        name: 'jack',                                                   //property 1
+        getHand: getHand                                                //property 2
     },
 }
 
@@ -23,19 +24,22 @@ function playRound() {
     console.log(`${players.jack.name} plays ${jackHand}`);                     
     
 
-if (deanoHand === jackHand) {
-    return ("DRAW");
-} else if (
-    (deanoHand === 'rock' && jackHand === 'paper') ||
-    (deanoHand === 'paper' && jackHand === 'scissors') ||
-    (deanoHand === 'scissors'&& jackHand === 'rock') 
-    
- ) {
-    return ('Jack WINS!')
- }
-    else {
-    return ('Deano WINS!')
+    if (deanoHand === jackHand) {                                       //returns draw if same hands are played
+        return "DRAW";
+    } else if (                                                         //returns jack wins if deano plays lesser hands
+        (deanoHand === 'rock' && jackHand === 'paper') ||
+        (deanoHand === 'paper' && jackHand === 'scissors') ||
+        (deanoHand === 'scissors'&& jackHand === 'rock') 
+        
+    ) {
+        return 'Jack WINS!'
     }
+    else {                                                              //returns deano wins if jack plays lesser hands
+        return 'Deano WINS!'
+        }
 
 }
+
+const result = playRound();                                             //placed at the bottom as the variable can only be called after the function "playRound" is defined
+console.log(result);                                                    
 
