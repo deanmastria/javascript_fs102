293b1975c58022986d1a26edef5cc89fc3c8b444
deanmastria <mastriadata@gmail.com>
2024-06-13 11:50:15 -0400

    funstion palyRound test

--- RockPaperScissorp1.js
+++ RockPaperScissorp1.js
@@ -1,22 +1,26 @@
-const hands = ['rock', 'paper', 'scissors'];
+const hands = ['rock', 'paper', 'scissors']; //an array that creates rock papaer scissors as the constant variables
 
 function getHand(){
-    return hands[parseInt(math.random()*10)%3]
+    return hands[parseInt(math.random()*10)%3] //returns hands from the array
 }
 
-let players = {
-    deano: {
-        name: 'deano',
-        getHand: getHand
+let players = {                               //variable
+    deano: {                                  //object 1
+        name: 'deano',                        //property 1
+        getHand: getHand                      //property 2
     },
-    jack: {
-        name: 'jack'
-        getHand: getHand
+    jack: {                                   //object 2
+        name: 'jack'                          //property 1
+        getHand: getHand                      //property 2
     },
 }
 
 
 function playRound() {
+    const player1Hand = players.deano.getHand();
+    const player2Hand = players.jack.getHand();
+    console.log(`$players.name.deano + plays $player1Hand);
+    console.log(`$players.name.jack + plays $player2Hand);                     
     
 }
 
